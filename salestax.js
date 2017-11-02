5081a987d4f48d739cc0bfb449f12f47f18737de
Johnnie Regalado <johnniereg@gmail.com>
2017-11-02 01:57:51 +0000

    figured out how to add company names to expected results object

---
+++ salestax.js
@@ -0,0 +1,77 @@
+var salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+// an array filled with objects
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+
+function calculateSalesTax(salesData, taxRates) {
+  var expectedResults = {};
+
+  // add the company names to our results object
+  for (i = 0; i < salesData.length; i++) {
+    var companyName = salesData[i].name;
+    expectedResults[companyName] = {};
+    // expectedResults["salesData[i]"] = {};
+  }
+
+  console.log(expectedResults);
+
+
+
+}
+// access parts of each item in array of objects
+// can access with . notation or [""] notation
+// console.log(companySalesData[0].name);
+// console.log(companySalesData[0]["province"]);
+// console.log(companySalesData[0]["sales"]);
+
+//access the objects in this array like this
+// companySalesData[0]; --> Telus BC
+// companySalesData[1]; --> Bombardier AB
+// companySalesData[2]; --> Telus SK
+
+
+// function calculateTotalSales () {
+//   for ()
+
+// }
+
+// function calculateTax () {
+
+// }
+
+var results = calculateSalesTax(companySalesData, salesTaxRates);
+
+
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
